259a448a009a437671d7b9540ce1c5563c02cd6b
tharun2258 <yaminik114@gmail.com>
2025-02-25 17:19:36 +0530

    updated files

--- AWS_EKS/eks_cluster.tf
+++ AWS_EKS/eks_cluster.tf
@@ -1,12 +1,12 @@
 resource "aws_eks_cluster" "eks_cluster" {
-    name = "demo-eks-cluster-terraform"
+    name = var.eks_cluster_name
     role_arn = aws_iam_role.eks_cluster_role.arn
     vpc_config {
       subnet_ids = ["subnet ids of 2 different AZ's"]
     }
 
     tags = {
-      Name = "eks-demo"
+      Name = var.eks_cluster_tag_name
     }
   
 }
